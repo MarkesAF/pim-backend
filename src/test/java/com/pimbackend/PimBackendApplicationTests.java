7018d5c45810a171437005080dfa0138c87853ba
Augusto Marques <markesaf16@gmail.com>
2023-09-25 22:05:37 -0300

    Iniciando o projeto

---
+++ src/test/java/com/pimbackend/PimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
