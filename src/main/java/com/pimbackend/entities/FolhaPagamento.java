c9751a626fce6d4512263b7052dbf0e6ad99665d
Augusto Marques <markesaf16@gmail.com>
2023-10-23 21:57:57 -0300

    Implementando Lombok

--- src/main/java/com/pimbackend/entities/FolhaPagamento.java
+++ src/main/java/com/pimbackend/entities/FolhaPagamento.java
@@ -3,71 +3,31 @@ package com.pimbackend.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import com.pimbackend.entities.Funcionario;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "tbfolhapagamento",schema = "Oasis")
 @EqualsAndHashCode(of = "id")
 public class FolhaPagamento {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private Double salario;
+
     private Double descontos;
+
     private Double bonus;
+
     @OneToOne
     @JoinColumn(name = "funcionario_id", referencedColumnName = "id")
     private Funcionario funcionario;
-
-    public FolhaPagamento(){
-    }
-    public FolhaPagamento(Long id, Double salario, Double descontos, Double bonus, Funcionario funcionario){
-        this.id = id;
-        this.salario = salario;
-        this.descontos = descontos;
-        this.bonus = bonus;
-        this.funcionario = funcionario;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(Double salario) {
-        this.salario = salario;
-    }
-
-    public Double getDescontos() {
-        return descontos;
-    }
-
-    public void setDescontos(Double descontos) {
-        this.descontos = descontos;
-    }
-
-    public Double getBonus() {
-        return bonus;
-    }
-
-    public void setBonus(Double bonus) {
-        this.bonus = bonus;
-    }
-
-    public Funcionario getFuncionario() {
-        return funcionario;
-    }
-
-    public void setFuncionario(Funcionario funcionario) {
-        this.funcionario = funcionario;
-    }
 }
 
--- src/main/java/com/pimbackend/entities/Funcionario.java
+++ src/main/java/com/pimbackend/entities/Funcionario.java
@@ -3,71 +3,32 @@ package com.pimbackend.entities;
 
 import com.pimbackend.dto.FuncionarioDTO;
 import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Table(name = "tbfuncionario", schema = "Oasis")
 public class Funcionario {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull @NotBlank
     private String nome;
+
     @Column(unique = true, nullable = false, length = 11)
     private String cpf;
+
     @Column(nullable = false)
     private String cargo;
+
+    @NotNull
     private Double salario;
-
-
-    public Funcionario(){
-    }
-    public Funcionario(Long id, String nome, String cpf, String cargo, Double salario){
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.salario = salario;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public Double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(Double salario) {
-        this.salario = salario;
-    }
-
 }
